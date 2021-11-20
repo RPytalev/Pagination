7bdd87ccbaac46adf22c85261149e98707a0a328
Roman Pytalev <rpytalev@gmail.com>
2021-11-20 06:59:08 +0300

    "delete data from index.js"

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,6 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    // goods: [
-    //   { id: 1, type: 'Book', name: 'Peace', price: 100 },
-    //   { id: 2, type: 'Car', name: 'Tesla', price: 100000 },
-    //   { id: 3, type: 'Burger', name: 'Wopper', price: 10 },
-    //   { id: 4, type: 'Lemonade', name: 'Orange', price: 1 },
-    //   { id: 5, type: 'Pen', name: 'Parker', price: 500 }
-    // ]
   },
   mutations: {
   },
